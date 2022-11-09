7d90ec7befdc9d403976ca3309f4bf4873549028
nascimento92 <gabriel_maker@hotmail.com>
2022-11-09 14:41:19 -0300

    a

--- server/infra/database.js
+++ server/infra/database.js
@@ -2,7 +2,8 @@ const pgp = require('pg-promise')();
 const db = pgp({
     user: 'postgres',
     password: 'VnBgPQbYzwa95VDm',
-    host: '34.95.183.152',
+ // host: '34.95.183.152',
+    host: '10.92.160.5',
     port: '5432',
     database:'TelemetriaGC',
     allowExitOnIdle: true
